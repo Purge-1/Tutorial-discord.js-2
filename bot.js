95ca23314890751a043898322df301066098c9a7
Purge-1 <80547719+Purge-1@users.noreply.github.com>
2021-03-31 11:31:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,3 +20,24 @@ client.on('message', message => {
 	}
 })
 client.login("token")
+/////////////////////////////////////////////////////////////
+// This is now for webhooks
+'use strict';
+
+/**
+ * Send a message using a webhook
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+/*
+ * Create a new webhook
+ * The Webhooks ID and token can be found in the URL, when you request that URL, or in the response body.
+ * https://discord.com/api/webhooks/12345678910/T0kEn0fw3Bh00K
+ *                                  ^^^^^^^^^^  ^^^^^^^^^^^^ 
+ *                                  Webhook ID  Webhook Token
+ */
+const hook = new Discord.WebhookClient('webhook id', 'webhook token');
+
+// Send a message using the webhook
+hook.send('I am now alive!');
